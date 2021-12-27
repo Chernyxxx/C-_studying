3b27e4099f54d6f2036c4195a7f9da8e33825ca7
Dmitry Chernoivanov <chernyxxx@mail.ru>
2021-12-27 18:15:30 +0300

    file added

---
+++ 03_Triangle_in_console/03_Triangle_in _console.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Triangle_in_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (; i<10; i++ )
+            {
+
+                // Вложенный цикл for имеет 2 условия выхода
+
+                for (int j=0; j<10 && j<(10-i); j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
