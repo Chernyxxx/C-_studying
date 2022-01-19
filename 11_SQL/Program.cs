6537b2009cdcc215acb7e8dfc72ce8f1c9a42907
Dmitry Chernoivanov <chernyxxx@mail.ru>
2022-01-19 07:01:30 +0300

    DB added

--- 11_SQL/Program.cs
+++ 11_SQL/Program.cs
@@ -22,22 +22,62 @@ namespace _11_SQL
         {
             //
             //              1. Создание базы данных
-            // К проекту Добавить -> Создать элемент ->
+            // 1 вариант. К проекту Добавить -> Создать элемент ->
             // Базы данных основанные на службах.
+            // 2 вариант см. ниже
             //
+            // Подключаемся к MSSQLServer к базе данных master, которая
+            // по умолчанию предустановлена на любом сервере
             //
-            //              2. Подключение к базе данных
-            //              
-            
-            string connectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = E:\\C#\\11_SQL\\Database1.mdf;Integrated Security=True";
 
-            SqlConnection connection = new SqlConnection(connectionString);
+            SqlConnection connection = new SqlConnection("Server = (localdb)\\mssqllocaldb;Database = master;Trusted_Connection=True;");
             try
             {
+                connection.Open();
+                Console.WriteLine("Подключение к БД master");
+                //
+                // Создание новой базы данныз
+                //
+                SqlCommand command = new SqlCommand("CREATE DATABASE Database2", connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("База данных Database2 создана");
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if(connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            Console.WriteLine("Подключение закрыто");
+            
+           /*
+
+            // Выполнение команды 
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("База данных создана");
+
+
+            // 
+            //              2. Подключение к базе данных
+            //              
+            //              
+
+            string connectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = E:\\C#\\11_SQL\\Database1.mdf;Integrated Security=True";
+
+            using (SqlConnection connection1 = new SqlConnection(connectionString))
+            {
+                // Подключение    
+                connection.Open();
                 //
                 // Свойства  SqlConnection
                 //
-                connection.Open();
+
                 Console.WriteLine("Подключение открыто");
                 Console.WriteLine($"Строка подключения \n { connection.ConnectionString}");
                 Console.WriteLine($"База данных \t {connection.Database}");
@@ -47,29 +87,24 @@ namespace _11_SQL
                 Console.WriteLine($"Сервер WorkstationId \t { connection.WorkstationId}");
 
                 //
-                //      3. Команды, создание таблицы.
+                //      3. Команды, создание таблицы
                 // 
 
-                SqlCommand command = new SqlCommand();
-                command.CommandText = "CREATE TABLE Staff(Id INT PRIMARY KEY IDENTITY, LOGIN NVARCHAR(32) NOT NULL, PASSWORD NVARCHAR(32) NOT NULL)";
-                command.Connection = connection;
+                
+
+                SqlCommand command1 = new SqlCommand();
+                command1.CommandText = "CREATE TABLE Staff(Id INT PRIMARY KEY IDENTITY, LOGIN NVARCHAR(32) NOT NULL, PASSWORD NVARCHAR(32) NOT NULL)";
+                command1.Connection = connection;
 
                 // Выполнение команды
-                command.ExecuteNonQuery();
+                command1.ExecuteNonQuery();
+
+                Console.WriteLine("Таблица создана");
+
             }
-            catch(SqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                if (connection.State == ConnectionState.Open)
-                {
-                    connection.Close();
-                    Console.WriteLine("Подключение закрыто");
-                }
-            }
-            Console.WriteLine("Программа завершила работу");
+
+         */
+            
         }
     }
 }
