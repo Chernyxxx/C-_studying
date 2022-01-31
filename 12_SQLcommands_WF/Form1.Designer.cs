4d4588488e6b27a0b766c416fb1b078e915dc430
Dmitry Chernoivanov <chernyxxx@mail.ru>
2022-01-31 12:07:37 +0300

    changed

--- 12_SQLcommands_WF/Form1.Designer.cs
+++ 12_SQLcommands_WF/Form1.Designer.cs
@@ -51,32 +51,40 @@ namespace _12_SQLcommands_WF
             this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.checkBox_delete = new System.Windows.Forms.CheckBox();
+            this.checkBox_update = new System.Windows.Forms.CheckBox();
+            this.checkBox_insert = new System.Windows.Forms.CheckBox();
+            this.checkBox_search = new System.Windows.Forms.CheckBox();
+            this.showAll_butt = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.showAll_butt);
             this.panel1.Controls.Add(this.listBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(357, 24);
+            this.panel1.Location = new System.Drawing.Point(367, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(370, 244);
+            this.panel1.Size = new System.Drawing.Size(379, 296);
             this.panel1.TabIndex = 1;
             // 
             // listBox1
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(370, 244);
+            this.listBox1.Size = new System.Drawing.Size(379, 199);
             this.listBox1.TabIndex = 0;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(79, 181);
+            this.label4.Location = new System.Drawing.Point(93, 231);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 24;
@@ -85,7 +93,7 @@ namespace _12_SQLcommands_WF
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(80, 139);
+            this.label3.Location = new System.Drawing.Point(94, 205);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 25;
@@ -94,7 +102,7 @@ namespace _12_SQLcommands_WF
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(82, 93);
+            this.label2.Location = new System.Drawing.Point(96, 179);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 26;
@@ -103,7 +111,7 @@ namespace _12_SQLcommands_WF
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(102, 53);
+            this.label1.Location = new System.Drawing.Point(116, 153);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 27;
@@ -111,35 +119,40 @@ namespace _12_SQLcommands_WF
             // 
             // lName_textBox
             // 
-            this.lName_textBox.Location = new System.Drawing.Point(176, 181);
+            this.lName_textBox.Enabled = false;
+            this.lName_textBox.Location = new System.Drawing.Point(171, 228);
             this.lName_textBox.Name = "lName_textBox";
             this.lName_textBox.Size = new System.Drawing.Size(100, 20);
             this.lName_textBox.TabIndex = 20;
             // 
             // fName_textBox
             // 
-            this.fName_textBox.Location = new System.Drawing.Point(176, 139);
+            this.fName_textBox.Enabled = false;
+            this.fName_textBox.Location = new System.Drawing.Point(171, 202);
             this.fName_textBox.Name = "fName_textBox";
             this.fName_textBox.Size = new System.Drawing.Size(100, 20);
             this.fName_textBox.TabIndex = 21;
             // 
             // password_textBox
             // 
-            this.password_textBox.Location = new System.Drawing.Point(176, 93);
+            this.password_textBox.Enabled = false;
+            this.password_textBox.Location = new System.Drawing.Point(171, 176);
             this.password_textBox.Name = "password_textBox";
             this.password_textBox.Size = new System.Drawing.Size(100, 20);
             this.password_textBox.TabIndex = 22;
             // 
             // login_textBox
             // 
-            this.login_textBox.Location = new System.Drawing.Point(176, 53);
+            this.login_textBox.Enabled = false;
+            this.login_textBox.Location = new System.Drawing.Point(171, 150);
             this.login_textBox.Name = "login_textBox";
             this.login_textBox.Size = new System.Drawing.Size(100, 20);
             this.login_textBox.TabIndex = 23;
             // 
             // search_butt
             // 
-            this.search_butt.Location = new System.Drawing.Point(257, 230);
+            this.search_butt.Enabled = false;
+            this.search_butt.Location = new System.Drawing.Point(277, 276);
             this.search_butt.Name = "search_butt";
             this.search_butt.Size = new System.Drawing.Size(75, 23);
             this.search_butt.TabIndex = 16;
@@ -149,7 +162,8 @@ namespace _12_SQLcommands_WF
             // 
             // update_butt
             // 
-            this.update_butt.Location = new System.Drawing.Point(176, 230);
+            this.update_butt.Enabled = false;
+            this.update_butt.Location = new System.Drawing.Point(196, 276);
             this.update_butt.Name = "update_butt";
             this.update_butt.Size = new System.Drawing.Size(75, 23);
             this.update_butt.TabIndex = 17;
@@ -159,7 +173,8 @@ namespace _12_SQLcommands_WF
             // 
             // delete_butt
             // 
-            this.delete_butt.Location = new System.Drawing.Point(95, 230);
+            this.delete_butt.Enabled = false;
+            this.delete_butt.Location = new System.Drawing.Point(115, 276);
             this.delete_butt.Name = "delete_butt";
             this.delete_butt.Size = new System.Drawing.Size(75, 23);
             this.delete_butt.TabIndex = 18;
@@ -169,7 +184,8 @@ namespace _12_SQLcommands_WF
             // 
             // insert_butt
             // 
-            this.insert_butt.Location = new System.Drawing.Point(12, 230);
+            this.insert_butt.Enabled = false;
+            this.insert_butt.Location = new System.Drawing.Point(32, 276);
             this.insert_butt.Name = "insert_butt";
             this.insert_butt.Size = new System.Drawing.Size(75, 23);
             this.insert_butt.TabIndex = 28;
@@ -179,7 +195,8 @@ namespace _12_SQLcommands_WF
             // 
             // id_textBox
             // 
-            this.id_textBox.Location = new System.Drawing.Point(176, 27);
+            this.id_textBox.Enabled = false;
+            this.id_textBox.Location = new System.Drawing.Point(171, 124);
             this.id_textBox.Name = "id_textBox";
             this.id_textBox.Size = new System.Drawing.Size(100, 20);
             this.id_textBox.TabIndex = 29;
@@ -187,7 +204,7 @@ namespace _12_SQLcommands_WF
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(119, 27);
+            this.label5.Location = new System.Drawing.Point(133, 127);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(16, 13);
             this.label5.TabIndex = 30;
@@ -235,16 +252,94 @@ namespace _12_SQLcommands_WF
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(727, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(746, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // checkBox_delete
+            // 
+            this.checkBox_delete.AutoSize = true;
+            this.checkBox_delete.Location = new System.Drawing.Point(106, 70);
+            this.checkBox_delete.Name = "checkBox_delete";
+            this.checkBox_delete.Size = new System.Drawing.Size(57, 17);
+            this.checkBox_delete.TabIndex = 31;
+            this.checkBox_delete.Text = "Delete";
+            this.checkBox_delete.UseVisualStyleBackColor = true;
+            this.checkBox_delete.CheckedChanged += new System.EventHandler(this.checkBox_delete_CheckedChanged);
+            // 
+            // checkBox_update
+            // 
+            this.checkBox_update.AutoSize = true;
+            this.checkBox_update.Location = new System.Drawing.Point(187, 69);
+            this.checkBox_update.Name = "checkBox_update";
+            this.checkBox_update.Size = new System.Drawing.Size(61, 17);
+            this.checkBox_update.TabIndex = 31;
+            this.checkBox_update.Text = "Update";
+            this.checkBox_update.UseVisualStyleBackColor = true;
+            this.checkBox_update.CheckedChanged += new System.EventHandler(this.checkBox_update_CheckedChanged);
+            // 
+            // checkBox_insert
+            // 
+            this.checkBox_insert.AutoSize = true;
+            this.checkBox_insert.Location = new System.Drawing.Point(32, 69);
+            this.checkBox_insert.Name = "checkBox_insert";
+            this.checkBox_insert.Size = new System.Drawing.Size(52, 17);
+            this.checkBox_insert.TabIndex = 31;
+            this.checkBox_insert.Text = "Insert";
+            this.checkBox_insert.UseVisualStyleBackColor = true;
+            this.checkBox_insert.CheckedChanged += new System.EventHandler(this.checkBox_insert_CheckedChanged);
+            // 
+            // checkBox_search
+            // 
+            this.checkBox_search.AutoSize = true;
+            this.checkBox_search.Location = new System.Drawing.Point(264, 68);
+            this.checkBox_search.Name = "checkBox_search";
+            this.checkBox_search.Size = new System.Drawing.Size(60, 17);
+            this.checkBox_search.TabIndex = 32;
+            this.checkBox_search.Text = "Search";
+            this.checkBox_search.UseVisualStyleBackColor = true;
+            this.checkBox_search.CheckedChanged += new System.EventHandler(this.checkBox_search_CheckedChanged);
+            // 
+            // showAll_butt
+            // 
+            this.showAll_butt.Location = new System.Drawing.Point(142, 236);
+            this.showAll_butt.Name = "showAll_butt";
+            this.showAll_butt.Size = new System.Drawing.Size(75, 23);
+            this.showAll_butt.TabIndex = 1;
+            this.showAll_butt.Text = "SHOW ALL";
+            this.showAll_butt.UseVisualStyleBackColor = true;
+            this.showAll_butt.Click += new System.EventHandler(this.showAll_butt_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "1.  Выберите действие";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 13);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "2.  Заполните поля";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 268);
+            this.ClientSize = new System.Drawing.Size(746, 320);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox_search);
+            this.Controls.Add(this.checkBox_insert);
+            this.Controls.Add(this.checkBox_update);
+            this.Controls.Add(this.checkBox_delete);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.id_textBox);
             this.Controls.Add(this.insert_butt);
@@ -296,6 +391,13 @@ namespace _12_SQLcommands_WF
         private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.CheckBox checkBox_delete;
+        private System.Windows.Forms.CheckBox checkBox_update;
+        private System.Windows.Forms.CheckBox checkBox_insert;
+        private System.Windows.Forms.CheckBox checkBox_search;
+        private System.Windows.Forms.Button showAll_butt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- 12_SQLcommands_WF/Form1.cs
+++ 12_SQLcommands_WF/Form1.cs
@@ -20,11 +20,102 @@ namespace _12_SQLcommands_WF
         {
             InitializeComponent();
         }
+        //
+        //                       ЧЕКБОКСЫ
+        //
+        //
+        //             
 
+        private void checkBox_insert_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox_delete.Checked = false;
+            checkBox_update.Checked = false;
+            checkBox_search.Checked = false;
+
+            insert_butt.Enabled = true;
+            delete_butt.Enabled = false;
+            update_butt.Enabled = false;
+            search_butt.Enabled = false;
+
+            id_textBox.Enabled = false;
+            login_textBox.Enabled = true;
+            password_textBox.Enabled = true;
+            fName_textBox.Enabled = true;
+            lName_textBox.Enabled = true;
+        }
+        private void checkBox_delete_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_delete.Checked == true)
+            {
+                checkBox_insert.Checked = false;
+                checkBox_update.Checked = false;
+                checkBox_search.Checked = false;
+
+                delete_butt.Enabled = true;
+                insert_butt.Enabled = false;
+                update_butt.Enabled = false;
+                search_butt.Enabled = false;
+
+                id_textBox.Enabled = true;
+                login_textBox.Enabled = false;
+                password_textBox.Enabled = false;
+                fName_textBox.Enabled = false;
+                lName_textBox.Enabled = false;
+            }
+        }
+
+        private void checkBox_update_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_update.Checked == true)
+            {
+                checkBox_insert.Checked = false;
+                checkBox_delete.Checked = false;
+                checkBox_search.Checked = false;
+
+                update_butt.Enabled = true;
+                insert_butt.Enabled = false;
+                delete_butt.Enabled = false;
+                search_butt.Enabled = false;
+
+                id_textBox.Enabled = true;
+                login_textBox.Enabled = true;
+                password_textBox.Enabled = true;
+                fName_textBox.Enabled = true;
+                lName_textBox.Enabled = true;
+            }
+                
+        }
+
+        private void checkBox_search_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_search.Checked == true)
+            {
+                checkBox_insert.Checked = false;
+                checkBox_delete.Checked = false;
+                checkBox_update.Checked = false;
+
+                search_butt.Enabled = true;
+                insert_butt.Enabled = false;
+                delete_butt.Enabled = false;
+                update_butt.Enabled = false;
+
+                id_textBox.Enabled = true;
+                login_textBox.Enabled = true;
+                password_textBox.Enabled = false;
+                fName_textBox.Enabled = true;
+                lName_textBox.Enabled = true;
+            }
+                
+        }
+        //
+        //               СОЕДИНЕНИЕ И ЗАПОЛНЕНИЕ ЛИСТБОКСА
+        //
+        //
         private void Form1_Load(object sender, EventArgs e)
         {
             connection = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = E:\\C#\\12_SQLcommands_WF\\Database1.mdf;Integrated Security=True");
             connection.Open();
+
             if (connection.State==ConnectionState.Open)
             {
                // MessageBox.Show("Соединение установлено"); 
@@ -40,11 +131,10 @@ namespace _12_SQLcommands_WF
             try
             {
                 reader = command.ExecuteReader();
-               // string colomns = $"{reader.GetName(0)}\t \t{reader.GetName(1)}\t\t {reader.GetName(3)}\t {reader.GetName(4)}";
-               // listBox1.Items.Add(colomns);
+              
                 while (reader.Read())
                 {
-                    string values = $"{ reader["Id"]}\t\t { reader["Login"]}\t \t{reader["First_name"]} \t\t{reader["Last_name"]}  ";
+                    string values = $"{ reader["Id"]}\t { reader["Login"]}\t \t{reader["First_name"]}\t\t{reader["Last_name"]}  ";
                     listBox1.Items.Add(values);
                 }
             }
@@ -56,11 +146,15 @@ namespace _12_SQLcommands_WF
             {
                 if (reader!= null)
                     reader.Close();
-            }
-
-            
+            }    
         }
-
+        //
+        //                  ОБРАБОТКА НАЖАТИЯ КНОПОК
+        //
+        //
+        //
+        //                      Кнопка INSERT 
+        //
         private void insert_butt_Click(object sender, EventArgs e)
         {
             string cmdText = $"INSERT INTO Users (Login, Password, First_name, Last_name) VALUES ('{login_textBox.Text}', '{password_textBox.Text}','{fName_textBox.Text}', '{lName_textBox.Text}')";
@@ -76,28 +170,63 @@ namespace _12_SQLcommands_WF
             {
                 MessageBox.Show(ex.Message);
             }
+            
+            login_textBox.Text = "";
+            password_textBox.Text = "";
+            fName_textBox.Text = "";
+            lName_textBox.Text = "";
         }
-
+        //
+        //           Кнопка DELETE - удаление по Id
+        //
         private void delete_butt_Click(object sender, EventArgs e)
         {
-            string cmdText = $"DELETE FROM Users WHERE Login ='{login_textBox.Text}'";
+            int id = Convert.ToInt32(id_textBox.Text);
+            string cmdText = $"DELETE FROM Users WHERE Id ={id}";
             SqlCommand command = new SqlCommand(cmdText, connection);
             
             command.ExecuteNonQuery();
             MessageBox.Show($"Пользователь '{login_textBox.Text}' удалён");
-            
+            id_textBox.Text = "";
         }
+        //
+        //              Кнопка UPDATE - обновить по Id
+        //
+        private void update_butt_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(id_textBox.Text);
+            string cmdText = $"UPDATE Users SET Login = '{login_textBox.Text}', Password ='{password_textBox.Text}', First_name = '{fName_textBox.Text}', Last_name ='{lName_textBox.Text}' WHERE Id ={id}";
+            SqlCommand command = new SqlCommand(cmdText, connection);
 
+            command.ExecuteNonQuery();
+            MessageBox.Show($"Изменены данные пользователя Id = '{id_textBox.Text}'");
+
+            id_textBox.Text = "";
+            login_textBox.Text = "";
+            password_textBox.Text = "";
+            fName_textBox.Text = "";
+            lName_textBox.Text = "";
+        }
+        //
+        //              Кнопка SEARCH - обновить по любым данным
+        //
         private void search_butt_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            string cmdText = $"SELECT * FROM Users WHERE Login='{login_textBox.Text}' OR First_name = '{fName_textBox.Text}' OR Last_name = '{lName_textBox.Text}'";
+
+            int id = 0;
+
+            if (id_textBox.Text !="")
+            {
+                id = Convert.ToInt32(id_textBox.Text);
+            }
+            
+            string cmdText = $"SELECT * FROM Users WHERE Id={id} OR Login='{login_textBox.Text}' OR First_name = '{fName_textBox.Text}' OR Last_name = '{lName_textBox.Text}'";
             SqlCommand command = new SqlCommand(cmdText, connection);
             SqlDataReader reader = command.ExecuteReader();
             if (reader.HasRows)
             {
-                // string colomns = $"{reader.GetName(0)}\t \t{reader.GetName(1)}\t\t {reader.GetName(3)}\t {reader.GetName(4)}";
-                // listBox1.Items.Add(colomns);
+                
                 while (reader.Read())
                 {
                     string values = $"{ reader["Id"]}\t\t { reader["Login"]}\t \t{reader["First_name"]} \t\t{reader["Last_name"]}  ";
@@ -111,19 +240,42 @@ namespace _12_SQLcommands_WF
             }
             reader.Close();
 
+            id_textBox.Text = "";
+            login_textBox.Text = "";
+            password_textBox.Text = "";
+            fName_textBox.Text = "";
+            lName_textBox.Text = "";
         }
 
-        private void update_butt_Click(object sender, EventArgs e)
+        //
+        //                     Кнопка SHOW ALL
+        //
+        private void showAll_butt_Click(object sender, EventArgs e)
         {
-            int id = Convert.ToInt32(id_textBox.Text);
-            string cmdText = $"UPDATE Users SET Login = '{login_textBox.Text}', Password ='{password_textBox.Text}', First_name = '{fName_textBox.Text}', Last_name ='{lName_textBox.Text}' WHERE Id ={id}";
+            listBox1.Items.Clear();
+            string cmdText = "SELECT * FROM Users ";
             SqlCommand command = new SqlCommand(cmdText, connection);
-           
-            command.ExecuteNonQuery();
-            MessageBox.Show($"Изменены данные пользователя Id = '{id_textBox.Text}'");
-                        
-        }
+            SqlDataReader reader = null;
+            try
+            {
+                reader = command.ExecuteReader();
 
+                while (reader.Read())
+                {
+                    string values = $"{ reader["Id"]}\t { reader["Login"]}\t \t{reader["First_name"]}\t\t{reader["Last_name"]}  ";
+                    listBox1.Items.Add(values);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
 
